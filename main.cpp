c0c1a5d2bd590f1b94b4e42253fad033c84e7b45
Ekremunalytu <ekremunalw1@gmail.com>
2023-08-01 00:05:12 +0300

    functions added

--- main.cpp
+++ main.cpp
@@ -5,16 +5,19 @@
 #include <thread>
 #include <fstream>
 
-
+void password_checker(std::string password , int *p_exit_pointer);
 
 int main() {
 
     std::string  password;
 
+    std::string *password_pointer = &password;
+
     int exit_flag = 1;
 
-    int length_flag = 0;
-    int lowercase_flag = 0 , uppercase_flag = 0 , number_flag = 0 , special_character_flag = 0;
+    int* p_exit_pointer = &exit_flag;
+    
+    
 
     std::ifstream file("passwords.txt");
 
@@ -36,10 +39,7 @@ int main() {
 
     while (exit_flag){
 
-        uppercase_flag = 0;
-        lowercase_flag = 0;
-        special_character_flag = 0;
-        number_flag = 0;
+        
         
         std::string line;
         file.clear();
@@ -48,7 +48,6 @@ int main() {
         while (std::getline(file ,line )){
             if ( line == password ){
                 std::cout << "Common password alert!" << std::endl;
-                std::cout << "Please do not use common passwords.These passwords create serious security flaws!!" << std::endl;
                 std::cout << "Please try again"  << std::endl;
                 file.clear();
                 file.seekg(0);
@@ -56,8 +55,32 @@ int main() {
             }
 
             }
+
+        password_checker(password , p_exit_pointer);
         
-        int password_length = password.length();
+        std::getline( std::cin , password);
+        
+    }
+
+
+        file.close();
+
+        std::this_thread::sleep_for(std::chrono::seconds(5));
+    
+    return 0;
+
+}
+
+
+
+void password_checker(std::string password  ,int  *p_exit_pointer ){
+
+    int uppercase_flag = 0;
+    int lowercase_flag = 0;
+    int special_character_flag = 0;
+    int number_flag = 0;
+
+    int password_length = password.length();
 
         if ( password_length < 8 ) {
             std::cout << "Password is too short!!! try again" << std::endl;
@@ -84,7 +107,7 @@ int main() {
         if  (number_flag && lowercase_flag && uppercase_flag && special_character_flag ){
             
             std::cout << "Well done. Your password is safe" << std::endl;
-            exit_flag = 0;
+            *p_exit_pointer = 0;
 
         } else{
            
@@ -107,15 +130,13 @@ int main() {
             } 
 
             std::cout << "Try again.. " << std::endl;
-            std::getline( std::cin , password);
+            
         }
 
     }
 
-    file.close();
 
-    std::this_thread::sleep_for(std::chrono::seconds(5));
-    
-    return 0;
 
-}
+
+
+
