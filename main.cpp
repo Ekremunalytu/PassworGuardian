0df7882ca6b1af50e3da7f58ca546cf365aa31de
Ekremunalytu <ekremunalw1@gmail.com>
2023-08-18 17:24:10 +0300

    password generator function added

--- main.cpp
+++ main.cpp
@@ -16,11 +16,18 @@ void password_checker(std::string password , int *p_exit_pointer);
 
 void brute_force_time(const std::string& password);
 
+std::string password_generator( int length);
+
 int main() {
 
     std::string  password;
+    std::string  generated_password;
 
     std::string *password_pointer = &password;
+    std::string *generated_password_pointer = &generated_password;
+    int password_selector;
+    int password_generation_flag = 1;
+    int generated_password_length;
 
     int exit_flag = 1;
 
@@ -34,8 +41,6 @@ int main() {
         std::cerr << "File could not opened!" << std::endl;
     }
 
-    
-    
 
     std::cout << "Welcome to Password Guardian" << std::endl;
     std::cout << "Please enter your password to see how safe it is" << std::endl;
@@ -45,7 +50,33 @@ int main() {
     std::cout << "3-Users must use at least 1 number and special character in their passwords" << std::endl;
     std::cout << "4-Users should not use common passwords. Our program detects common passwords and blocks users to obtain common passwords" << std::endl;
     std::cout << "5-Attackers can brute force your passwords if they have them. To prevent this , this program supports time complexity calculations." << std::endl;
-    std::getline(std::cin , password);
+    std::cout << "You can also use generated password from our program" << std::endl;
+    std::cout << "Enter 1 to generate password or enter 2 to enter your personal password" << std::endl;
+    std::cin  >> password_selector;
+
+    switch ( password_selector){
+    case 1:{
+        while (password_generation_flag){
+            std::cout << "Please enter password length" << std::endl;
+            std::cin  >> generated_password_length;
+            generated_password = password_generator(generated_password_length);
+            std::cout << "Generated password:  " << *generated_password_pointer << std::endl;
+            std::getline(std::cin , password);
+            std::cout << "Press 0 to proceed further " << std::endl;
+            std::cout << "Press 1 to generate password again" << std::endl;
+            std::cin >> password_generation_flag;
+        }
+        password = *generated_password_pointer;
+        break;
+    }
+    case 2:
+        break;
+    default:
+        std::cout << "Invalid choice" << std::endl;
+        return 1;
+    }
+    
+    
 
     while (exit_flag){
 
@@ -69,21 +100,14 @@ int main() {
         password_checker(password , p_exit_pointer);
 
         std::getline( std::cin , password);
-
-        
-        
+       
     }
 
-
         file.close();
-
-        
-    
+ 
     return 0;
 
 }
-
-
 
 void password_checker(std::string password  ,int  *p_exit_pointer ){
 
@@ -161,17 +185,43 @@ void brute_force_time(const std::string& password){
     double advanced_brute_force_time = ( total_calculations / advanced_brute_speed ) / 31536000;
 
     std::cout << std::fixed << std::setprecision(0);
-
+    std::cout << password << std::endl;
+    std::cout << "Estimated time to crack  password" << std::endl; 
     std::cout << "It takes " << basic_brute_force_time << " years to crack your password for basic brute force techniques" << std::endl;
     std::cout << "It takes " << average_brute_force_time << " years to crack your password for average brute force techniques" << std::endl;
     std::cout << "It takes " << advanced_brute_force_time << " years to crack your password for advanced brute force techniques" << std::endl;
 
     std::this_thread::sleep_for(std::chrono::seconds(5));
     exit(0);
-
-    
     
 }
 
 
+std::string password_generator ( int length){
+    srand(time(0));
 
+    const std::string lowercase = "abcdefghijklmnopqrstuvwxyz";
+    const std::string uppercase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    const std::string numbers = "0123456789";
+    const std::string special_characters = "!@#$%^&*()_-+=<>?";
+
+    std::string generated_password;
+
+    std::string valid_characters = lowercase + uppercase + numbers + special_characters;
+   
+    generated_password += lowercase[rand() % lowercase.length()];
+    generated_password += uppercase[rand() % uppercase.length()];
+    generated_password += numbers[rand() % numbers.length()];
+    generated_password += special_characters[rand() % special_characters.length()];
+
+    for ( int i = 0 ; i < length ; i++ ){
+        generated_password += valid_characters[rand() % valid_characters.length()];
+    }
+
+    for( int i = 0 ; i < length ;++i ){
+        int j = 0 ; j < rand() % length;
+        std::swap(generated_password[i] , generated_password[j]);
+    }
+
+    return generated_password;
+}
